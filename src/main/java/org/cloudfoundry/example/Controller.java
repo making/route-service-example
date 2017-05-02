7af9d3db0dc4d522e3badc01fb4ecf1be243b06a
making <makingx@gmail.com>
2017-05-03 04:19:11 +0900

    RouterFunction version

--- src/main/java/org/cloudfoundry/example/Controller.java
+++ src/main/java/org/cloudfoundry/example/Controller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,69 +16,82 @@
 
 package org.cloudfoundry.example;
 
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
 import java.net.URI;
 import java.util.Collections;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 import reactor.core.publisher.Mono;
-
 
 @RestController
 final class Controller {
 
-    static final String FORWARDED_URL = "X-CF-Forwarded-Url";
+	static final String FORWARDED_URL = "X-CF-Forwarded-Url";
 
-    static final String PROXY_METADATA = "X-CF-Proxy-Metadata";
+	static final String PROXY_METADATA = "X-CF-Proxy-Metadata";
 
-    static final String PROXY_SIGNATURE = "X-CF-Proxy-Signature";
+	static final String PROXY_SIGNATURE = "X-CF-Proxy-Signature";
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private final WebClient webClient;
+	private final WebClient webClient;
 
-    Controller() {
-        this.webClient = WebClient.create();
-    }
-
-	@RequestMapping(headers = { FORWARDED_URL, PROXY_METADATA, PROXY_SIGNATURE })
-	Mono<ResponseEntity<String>> service(RequestEntity<String> incoming) {
-        this.logger.info("Incoming Request: {}", incoming);
-
-        RequestEntity<?> outgoing = getOutgoingRequest(incoming);
-        this.logger.info("Outgoing Request: {}", outgoing);
-
-		WebClient.RequestHeadersSpec<?> spec = this.webClient.method(outgoing.getMethod())
-				.uri(outgoing.getUrl()).headers(outgoing.getHeaders());
-		if (outgoing.getBody() != null) {
-			spec = ((WebClient.RequestBodySpec) spec).syncBody(outgoing.getBody());
-		}
-		return spec.exchange()
-				.flatMap(x -> x.bodyToMono(String.class)
-						.map(s -> ResponseEntity.status(x.statusCode())
-								.headers(x.headers().asHttpHeaders()).body(s)));
+	Controller() {
+		this.webClient = WebClient.create();
 	}
 
-    private static RequestEntity<?> getOutgoingRequest(RequestEntity<?> incoming) {
+	public RouterFunction<ServerResponse> routes() {
+		return route(incoming(), this::service);
+	}
+
+	RequestPredicate incoming() {
+		return req -> {
+			HttpHeaders h = req.headers().asHttpHeaders();
+			return h.containsKey(FORWARDED_URL) && h.containsKey(PROXY_METADATA)
+					&& h.containsKey(PROXY_SIGNATURE);
+		};
+	}
+
+	Mono<ServerResponse> service(ServerRequest req) {
+		if (logger.isInfoEnabled()) {
+			logger.info("Incoming Request: <{} {},{}>", req.method(),
+					req.uri(), req.headers().asHttpHeaders());
+		}
+
+		HttpHeaders headers = headers(req.headers().asHttpHeaders());
+		URI uri = headers.remove(FORWARDED_URL).stream().findFirst().map(URI::create)
+				.orElseThrow(() -> new IllegalStateException(
+						String.format("No %s header present", FORWARDED_URL)));
+
+		if (logger.isInfoEnabled()) {
+			logger.info("Outgoing Request: <{} {},{}>", req.method(),
+					uri, headers);
+		}
+
+		Mono<String> incomingBody = req.bodyToMono(String.class)
+				.switchIfEmpty(Mono.empty());
+		return incomingBody.flatMap(body -> webClient.method(req.method()).uri(uri)
+				.headers(headers).syncBody(body).exchange()
+				.flatMap(x -> ServerResponse.status(x.statusCode())
+						.headers(x.headers().asHttpHeaders()).body(
+								x.bodyToMono(String.class), String.class)));
+	}
+
+	HttpHeaders headers(HttpHeaders incomingHeaders) {
 		HttpHeaders headers = new HttpHeaders();
-		HttpHeaders incomingHeaders = incoming.getHeaders();
 		headers.putAll(incomingHeaders);
 		String host = URI.create(incomingHeaders.getFirst(FORWARDED_URL)).getHost();
 		headers.put(HttpHeaders.HOST, Collections.singletonList(host));
-
-        URI uri = headers.remove(FORWARDED_URL).stream()
-            .findFirst()
-            .map(URI::create)
-            .orElseThrow(() -> new IllegalStateException(String.format("No %s header present", FORWARDED_URL)));
-
-        return new RequestEntity<>(incoming.getBody(), headers, incoming.getMethod(), uri);
-    }
-
+		return headers;
+	}
 }
--- src/main/java/org/cloudfoundry/example/RouteServiceApplication.java
+++ src/main/java/org/cloudfoundry/example/RouteServiceApplication.java
@@ -2,12 +2,19 @@ package org.cloudfoundry.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class RouteServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RouteServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RouteServiceApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routes(Controller controller) {
+		return controller.routes();
+	}
 }
